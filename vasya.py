db8b2996ba9b7a3a07e0823f15476ef2ef7a5255
OCHKOTUBERKULEZNOE <rignuliapuk182@mail.ru>
2022-10-24 21:23:37 +0400

    project start

---
+++ vasya.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+driver = webdriver.edge(executable_path="C:\\Users\\st\\Desktop\\Python\\pythontask2\\edgedriver_win64\\msedgedriver.exe")
+driver.
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element(By.NAME, "q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close()
+
+
+
+
