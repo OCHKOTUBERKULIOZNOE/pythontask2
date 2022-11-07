85b57ee76144dc057f100891ddf4d6a16add9806
OCHKOTUBERKULEZNOE <rignuliapuk182@mail.ru>
2022-11-07 21:33:29 +0400

    i am working on this

--- vasya.py
+++ vasya.py
@@ -1,5 +1,18 @@
+#импорт всякой шляпы
+import selenium
 from selenium import webdriver
 import sys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
-from time import sleep
+
+#запустить браузер (а надо запустить скрытно)
+browser = webdriver.Chrome()
+
+
+# browser.get('https://automatetheboringstuff.com')
+
+
+
+
+
+
